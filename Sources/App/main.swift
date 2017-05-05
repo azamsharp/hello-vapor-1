b1af400fc256ee145df290318e92152a746d3731
azamsharp <azamsharp@gmail.com>
2017-05-04 21:31:02 -0500

    vapor project

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,13 +1,12 @@
 import Vapor
+import VaporSQLite
 
 let drop = Droplet()
+try drop.addProvider(VaporSQLite.Provider.self)
 
-drop.get { req in
-    return try drop.view.make("welcome", [
-    	"message": drop.localization[req.lang, "welcome", "title"]
-    ])
+drop.get("version") { request in
+    let result = try drop.database?.driver.raw("SELECT sqlite_version()")
+    return try JSON(node: result)
 }
-
-drop.resource("posts", PostController())
 
 drop.run()
